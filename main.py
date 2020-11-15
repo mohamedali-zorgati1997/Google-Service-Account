53c1808a8dc68d7b608bc352af7f00535fdb784b
Mohamed Ali ZORGATI <46978677+mohamedali-zorgati1997@users.noreply.github.com>
2020-11-15 22:38:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from serviceaccount import ServiceAccount
+from tokenauth import ServiceAccountAuth
+
+from json import load
+from os import chdir
+from os.path import split
+from requests import get
+
+#I use this for testing environment where all the files are in one directory
+#I make sure the current directory is the file directory
+chdir(split(__file__)[0]) if split(__file__)[0] else None
+
+upload_url = 'https://www.googleapis.com/upload/drive/v3/'
+api_url = 'https://www.googleapis.com/drive/v3/'
+scopes = ['https://www.googleapis.com/auth/drive']
+
+SA = ServiceAccount(load(open('SA.json')))
+SA.setScopes(scopes)
+SA.readAT('AT_SA.pkl')
+
+auth = ServiceAccountAuth(SA)
+
+params = {
+    'fields': 'storageQuota/limit, storageQuota/usage'
+}
+resp = get(api_url + 'about', params=params, auth=auth)
+print(resp.status_code)
+storage = resp.json()['storageQuota']
+usage = int ((int(storage['usage']) / int(storage['limit'])) * 40)
+print('[' + '|' * usage + ' ' * (40 - usage) + ']' + '{}/{}'.format(storage['usage'], storage['limit']))
+
+SA.saveAT('AT_SA.pkl')
