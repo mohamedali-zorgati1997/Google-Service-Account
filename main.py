dc026b60ec5add3b195c3c215bb850cef9cbccf6
Mohamed Ali ZORGATI <46978677+mohamedali-zorgati1997@users.noreply.github.com>
2020-11-19 16:19:42 +0100

    add error handling
    add error handling to saveAT at the end of execution

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from json import load
 from os import chdir
 from os.path import split
 from requests import get
+from traceback import print_exc
 
 #I use this for testing environment where all the files are in one directory
 #I make sure the current directory is the file directory
@@ -20,13 +21,17 @@ SA.readAT('AT_SA.pkl')
 
 auth = ServiceAccountAuth(SA)
 
-params = {
-    'fields': 'storageQuota/limit, storageQuota/usage'
-}
-resp = get(api_url + 'about', params=params, auth=auth)
-print(resp.status_code)
-storage = resp.json()['storageQuota']
-usage = int ((int(storage['usage']) / int(storage['limit'])) * 40)
-print('[' + '|' * usage + ' ' * (40 - usage) + ']' + '{}/{}'.format(storage['usage'], storage['limit']))
-
-SA.saveAT('AT_SA.pkl')
+try:
+    
+    params = {
+        'fields': 'storageQuota/limit, storageQuota/usage'
+    }
+    resp = get(api_url + 'about', params=params, auth=auth)
+    print(resp.status_code)
+    storage = resp.json()['storageQuota']
+    usage = int ((int(storage['usage']) / int(storage['limit'])) * 40)
+    print('[' + '|' * usage + ' ' * (40 - usage) + ']' + '{}/{}'.format(storage['usage'], storage['limit']))
+except :
+    print_exc()
+finally:
+    SA.saveAT('AT_SA.pkl')
